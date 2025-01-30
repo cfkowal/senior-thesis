a5616b4dc0bc66d66576d48b4a4463e316e11948
Charles Kowalski <charlesfkowalski@gmail.com>
2025-01-30 12:37:10 -0500

    initial commit

---
+++ _ide/workspace_journal.py
@@ -0,0 +1,8 @@
+# 2025-01-30T12:30:49.845470800
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="thesis")
+
+comp = client.create_hls_component(name = "flash_attn",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
+
